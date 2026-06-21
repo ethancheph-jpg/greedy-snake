4a44626fc5c6e13ad335cba743219e18fee63703
ethancheph-jpg <ethan.che.ph@gmail.com>
2026-06-21 10:04:04 +0800

    greedy_download.py

--- greedy_download.py
+++ greedy_download.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Greedy Snake')
 
-URL = ''
+URL = 'https://drive.google.com/uc?export=download&id=1cibf-wvJ5fLfBfJzHg5HbtLAW_hZOUlo'
 
 st.link_button(
     label='download',
